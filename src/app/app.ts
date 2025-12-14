a0b9cd99a2b568053cd367974816c0bd7da07e2a
Estéfano Acevedo <acevedoestefano4@gmail.com>
2025-12-14 17:48:17 -0300

    fix(app.ts): Integrate router events to manage Bootstrap modal cleanup

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,16 @@ import { ReactiveFormsModule } from '@angular/forms';
 })
 export class App {
   protected title = 'EduRoom';
+
+  /* Limpieza total de Bootstrap */
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        document.body.classList.remove('modal-open');
+        document.body.style.overflow = 'auto';
+        document.querySelectorAll('.modal-backdrop')
+          .forEach(el => el.remove());
+      });
+  }
 }
